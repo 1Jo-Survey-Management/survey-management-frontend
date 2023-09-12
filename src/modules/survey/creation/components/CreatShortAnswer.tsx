728d4a9c259748364006f5f2ae5ec9a725cddfc2
gwanii <lung4536@gmail.com>
2023-09-12 09:47:22 +0900

    [feat]: 설문 작성 주관식 단답형 문항 컴포넌트 #1

--- src/modules/survey/creation/components/CreatShortAnswer.tsx
+++ src/modules/survey/creation/components/CreatShortAnswer.tsx
@@ -1,4 +1,4 @@
-import { Box, Input } from "@mui/material";
+import { Box, Input, TextField } from "@mui/material";
 import React from "react";
 
 const primaryColor = "#3f50b5";
@@ -7,8 +7,13 @@ const styles = {};
 
 const CreateShortAnswer: React.FC = () => {
   return (
-    <Box sx={{ display: "flex", flexGrow: "1" }}>
-      <Input disabled defaultValue={"문항 답변 입력란"}></Input>
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ width: "60.56px", marginRight: "10px" }}></Box>
+      <Input
+        disabled
+        defaultValue={"문항 답변 입력란"}
+        sx={{ flexGrow: "1", marginTop: "15px" }}
+      ></Input>
     </Box>
   );
 };
