04c26b7637c83ed0c1b8c1229d53efd0f33cf14d
DESKTOP-1IRTIK9\금정산2_PC22 <86181292+kl204@users.noreply.github.com>
2023-09-14 10:19:40 +0900

    [setting] eslint 설정 #2

--- src/modules/login/LoginDisplay.tsx
+++ src/modules/login/LoginDisplay.tsx
@@ -1,13 +1,12 @@
 // LoginDisplay.tsx
 import React, { useEffect } from 'react';
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import LoginFig from './img/LoginFig.png';
 import Logo from './img/SurveyLogo.png';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-
 
 import { styled } from '@mui/material/styles';
 
@@ -23,36 +22,32 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-
-
 const LoginDisplay: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const goLogin = () => {
-
-    console.log("nono");
+    console.log('nono');
     navigate('/survey/main');
-
   };
 
-  <CssBaseline/>
+  <CssBaseline />;
 
   const getNaverToken = () => {
     if (!location.hash) return;
     const token = location.hash.split('=')[1].split('&')[0];
     const state = location.hash.split('=')[2].split('&')[0];
-    console.log("AccessToken : "+token);
-    console.log("State : "+state);
+    console.log('AccessToken : ' + token);
+    console.log('State : ' + state);
   };
 
   useEffect(() => {
     getNaverToken();
-  }, []); 
+  }, []);
 
-  console.log(location.pathname)
+  console.log(location.pathname);
   return (
-
-    <Box component="div"
+    <Box
+      component="div"
       sx={{
         display: 'flex',
         position: 'absolute',
@@ -62,48 +57,59 @@ const LoginDisplay: React.FC = () => {
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        alignItems: "center", 
-        textAlign: "center",
-        justifyContent: "center" 
-
+        alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center',
       }}
     >
-
       <Box
         sx={{
           width: 550,
           height: 400,
           backgroundColor: '#C2E9FF',
-          borderRadius: '10px'
-
+          borderRadius: '10px',
         }}
       >
-        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Box
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
           <img src={`${Logo}`} style={{ width: '50px', marginRight: '10px' }} />
-          <h1 style={{ position: 'relative', color: '#9E9E9E' }}> NoName Survey</h1>
-        </Box>  
-
-        <Box style={{ height: '50px' }}>
-        </Box>  
-
-        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <LoginNaver/>
+          <h1 style={{ position: 'relative', color: '#9E9E9E' }}>
+            {' '}
+            NoName Survey
+          </h1>
         </Box>
 
-        <Box style={{ height: '50px' }}>
-        </Box> 
-        
+        <Box style={{ height: '50px' }}></Box>
 
-        <h2 style={{ position: 'relative', marginBottom: '1px' }}>Nice to See you Again</h2>
-        <Button style={{ position: 'relative', color: '#9E9E9E' }} onClick={goLogin}>비회원으로 로그인</Button>
+        <Box
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <LoginNaver />
+        </Box>
 
+        <Box style={{ height: '50px' }}></Box>
+
+        <h2 style={{ position: 'relative', marginBottom: '1px' }}>
+          Nice to See you Again
+        </h2>
+        <Button
+          style={{ position: 'relative', color: '#9E9E9E' }}
+          onClick={goLogin}
+        >
+          비회원으로 로그인
+        </Button>
       </Box>
-
-      
     </Box>
-
   );
 };
 
 export default LoginDisplay;
-
