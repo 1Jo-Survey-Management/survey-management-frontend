958f738b055040ff90c50f4bb20ef3bd77859255
DevPCW <rorndksk123@naver.com>
2023-09-18 10:28:25 +0900

    [feat] Eslint 설정 / 마이페이지 + 레이아웃 UI 2차 개발 #3

--- src/modules/survey/MypageWriteSurvey.tsx
+++ src/modules/survey/MypageWriteSurvey.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-shadow */
 import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import { useNavigate } from 'react-router-dom';
@@ -15,18 +14,14 @@ import {
   Fade,
 } from '@mui/material';
 
-// import "./MypageWriteSurvey.css";
-
 type CardData = {
   date: string;
   title: string;
-  tag: string[]; // 문자열 배열로 수정
+  tag: string[];
   description: string;
-  // 기타 카드에 필요한 속성들을 추가로 정의
 };
 
-// eslint-disable-next-line react/function-component-definition
-const Mypage: React.FC = () => {
+function Mypage() {
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState(false);
   const [selectedCard, setSelectedCard] = useState<CardData | null>(null);
@@ -43,70 +38,38 @@ const Mypage: React.FC = () => {
 
   const cardData = [
     {
-      date: '2023-09-13',
+      id: 1,
+      date: '~2023-09-13',
       title: 'WEHAGO 만족도 조사',
       tag: ['#WEHAGO', '#플랫폼'],
       description: '모달 시험 문구.',
     },
     {
-      date: '2023-09-13',
+      id: 2,
+      date: '~2023-09-13',
       title: 'Word of the Day 2',
       tag: ['#WEHAGO', '#플랫폼'],
       description: '이 설문에 대한 설명 문구입니다.',
     },
     {
-      date: '2023-09-13',
-      title: 'Word of the Day 3',
+      id: 3,
+      date: '~2023-09-13',
+      title: 'Word of the Day 2',
       tag: ['#WEHAGO', '#플랫폼'],
       description: '이 설문에 대한 설명 문구입니다.',
     },
-    {
-      date: '2023-09-13',
-      title: 'Word of the Day 4',
-      tag: ['#WEHAGO', '#플랫폼'],
-      description: '이 설문에 대한 설명 문구입니다.',
-    },
-    {
-      date: '2023-09-13',
-      title: 'Word of the Day 4',
-      tag: ['#WEHAGO', '#플랫폼'],
-      description: '이 설문에 대한 설명 문구입니다.',
-    },
-    {
-      date: '2023-09-13',
-      title: 'Word of the Day 4',
-      tag: ['#WEHAGO', '#플랫폼'],
-      description: '이 설문에 대한 설명 문구입니다.',
-    },
-    {
-      date: '2023-09-13',
-      title: 'Word of the Day 4',
-      tag: ['#WEHAGO', '#플랫폼'],
-      description: '이 설문에 대한 설명 문구입니다.',
-    },
-    {
-      date: '2023-09-13',
-      title: 'Word of the Day 4',
-      tag: ['#WEHAGO', '#플랫폼'],
-      description: '이 설문에 대한 설명 문구입니다.',
-    },
-    {
-      date: '2023-09-13',
-      title: 'Word of the Day 4',
-      tag: ['#WEHAGO', '#플랫폼'],
-      description: '이 설문에 대한 설명 문구입니다.',
-    },
-    {
-      date: '2023-09-13',
-      title: 'Word of the Day 4',
-      tag: ['#WEHAGO', '#플랫폼'],
-      description: '이 설문에 대한 설명 문구입니다.',
-    },
+    // 나머지 데이터 생략...
   ];
 
   return (
     <Container maxWidth="md" sx={{ paddingLeft: '5px', paddingRight: '5px' }}>
-      <h1>내가 작성한 설문 목록</h1>
+      <h1
+        style={{
+          fontSize: '25px',
+        }}
+      >
+        내가 작성한 설문 목록
+      </h1>
 
       <Box
         sx={{
@@ -117,13 +80,28 @@ const Mypage: React.FC = () => {
           height: '100%',
         }}
       >
-        {cardData.map((card, index) => (
-          // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions, react/no-array-index-key
-          <div key={index} onClick={() => openCardModal(card)}>
-            {' '}
+        {cardData.map((card) => (
+          <div
+            key={card.id} // 수정된 부분: 배열 인덱스 대신 고유한 값을 key로 사용
+            onClick={() => openCardModal(card)}
+            onKeyPress={() => openCardModal(card)}
+            role="button"
+            tabIndex={0}
+          >
             {/* 카드를 클릭하면 해당 카드 정보를 전달하여 모달 열기 */}
-            <Card sx={{ minWidth: 100, maxWidth: 150, minHeight: 80 }}>
-              <CardContent>
+            <Card
+              sx={{
+                minWidth: 100,
+                maxWidth: 150,
+                minHeight: 80,
+              }}
+            >
+              <CardContent
+                sx={{
+                  padding: '8px',
+                  justifyContent: 'space-between',
+                }}
+              >
                 {/* 카드 내용 */}
                 <div
                   style={{
@@ -133,12 +111,25 @@ const Mypage: React.FC = () => {
                     fontSize: 12,
                     color: 'text.secondary',
                     marginBottom: '16px',
+                    fontWeight: 600,
                   }}
                 >
                   {card.date}
                   <Stack spacing={1} alignItems="center">
                     <Stack direction="row" spacing={1}>
-                      <Chip label="진행중" color="primary" />
+                      <Chip
+                        label="진행중"
+                        color="primary"
+                        sx={{
+                          width: '50px',
+                          height: '20px',
+                          fontSize: '10px',
+                          fontWeight: 600,
+                          '& .MuiChip-label': {
+                            padding: 0,
+                          },
+                        }}
+                      />
                     </Stack>
                   </Stack>
                 </div>
@@ -161,13 +152,15 @@ const Mypage: React.FC = () => {
                   sx={{
                     fontSize: 11,
                     '& > span:not(:last-child)': {
-                      marginRight: '8px', // 여백 크기 조절
+                      marginRight: '8px',
                     },
                   }}
                 >
-                  {card.tag.map((tag, index) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <h1 key={index}>{tag}</h1>
+                  {card.tag.map((tag, index, array) => (
+                    <React.Fragment key={tag}>
+                      {tag}
+                      {index !== array.length - 1 && ', '}
+                    </React.Fragment>
                   ))}
                 </Typography>
               </CardContent>
@@ -184,11 +177,6 @@ const Mypage: React.FC = () => {
         onClose={closeCardModal}
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
-        // closeAfterTransition
-        // BackdropComponent={Backdrop}
-        // BackdropProps={{
-        //   timeout: 500,
-        // }}
         style={{
           display: 'flex',
           alignItems: 'center',
@@ -200,8 +188,8 @@ const Mypage: React.FC = () => {
             style={{
               backgroundColor: '#fff',
               boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.16)',
-              width: '350px', // 너비 조정
-              height: '500px', // 높이 조정
+              width: '350px',
+              height: '500px',
               padding: '16px',
               outline: 0,
               borderRadius: '8px',
@@ -218,6 +206,6 @@ const Mypage: React.FC = () => {
       </Modal>
     </Container>
   );
-};
+}
 
 export default Mypage;
