48bdb20fb6361eb1792f8528c7322b4fdd1abb1a
MyeoungDev <lung4536@gmail.com>
2023-09-26 16:27:27 +0900

    [feat]: class-validator 클래스 #1

---
+++ src/modules/survey/creation/validator/SelectionValidation.tsx
@@ -0,0 +1,42 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export default class SelectionValidation {
+  @IsNotEmpty({ message: '설문 문항 ID는 필수 입력사항입니다.' })
+  @IsNumber({}, { message: '설문 문항 ID는 숫자여야 합니다.' })
+  questionId: number;
+
+  @IsNotEmpty({ message: '선택지 ID는 필수 입력사항입니다.' })
+  @IsNumber({}, { message: '선택지 ID는 숫자여야 합니다.' })
+  selectionId: number;
+
+  @IsOptional()
+  questionMoveId?: number;
+
+  @IsNotEmpty({ message: '선택지 내용은 필수 입력사항입니다.' })
+  @IsString({ message: '선택지 내용은 문자열 입니다.' })
+  selectionValue: string;
+
+  @IsNotEmpty({ message: '선택지 이동여부는 필수 입력사항입니다.' })
+  @IsBoolean({ message: '선택지 이동여부는 Boolean 타입이어야 합니다.' })
+  isMoveable: boolean;
+
+  constructor(
+    questionId: number,
+    selectionId: number,
+    selectionValue: string,
+    isMoveable: boolean,
+    questionMoveId?: number
+  ) {
+    this.questionId = questionId;
+    this.selectionId = selectionId;
+    this.selectionValue = selectionValue;
+    this.isMoveable = isMoveable;
+    this.questionMoveId = questionMoveId;
+  }
+}
