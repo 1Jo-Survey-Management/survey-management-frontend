dbd767479651ece65f08177686fc00727b2dc136
PUO <86181292+kl204@users.noreply.github.com>
2023-10-13 15:10:28 +0900

    [fix] axios 설정 변경 #2

--- src/modules/layout/Layout.tsx
+++ src/modules/layout/Layout.tsx
@@ -5,7 +5,7 @@ import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
 import LoginPage from '../login/LoginDisplay';
-import axios from 'axios';
+import axios from '../login/components/customApi';
 
 /**
  * 페이지의 Header, Body, Footer를 모아놓은 Layout 입니다.
