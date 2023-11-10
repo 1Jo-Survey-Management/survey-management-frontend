4e3b08e615a31159983f9e7de4b057c9d701937c
PUO <86181292+kl204@users.noreply.github.com>
2023-11-10 09:38:52 +0900

    [fix] 주석 처리된 부분 활성화 #38

--- src/modules/survey/statistic/types/SurveyStatisticTypes.tsx
+++ src/modules/survey/statistic/types/SurveyStatisticTypes.tsx
@@ -1,17 +1,17 @@
-// import React from 'react';
+import React from 'react';
 
-// export interface Selection {
-//   surveyPostAt: string;
-//   userNickname: string;
-//   surveyNo: number;
-//   surveyTitle: string;
-//   surveyQuestionNo: number;
-//   surveyQuestionTitle: string;
-//   questionTypeNo: number;
-//   selectionNo: number;
-//   selectionValue: string;
-//   selectionCount: number;
-//   surveySubjectiveAnswer: string;
-//   surveySubjectiveAnswerCount: number;
-//   surveyWriter: string;
-// }
+export interface Selection {
+  surveyPostAt: string;
+  userNickname: string;
+  surveyNo: number;
+  surveyTitle: string;
+  surveyQuestionNo: number;
+  surveyQuestionTitle: string;
+  questionTypeNo: number;
+  selectionNo: number;
+  selectionValue: string;
+  selectionCount: number;
+  surveySubjectiveAnswer: string;
+  surveySubjectiveAnswerCount: number;
+  surveyWriter: string;
+}
