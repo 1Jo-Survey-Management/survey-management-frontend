329f73321905a1d6d5cd694676f0fefecbfd744d
ajinss <38906412+ajinss@users.noreply.github.com>
2023-11-22 16:38:53 +0900

    [design] : 모달 디자인 추가 #80

--- src/modules/survey/main/components/ClosingSurvey.tsx
+++ src/modules/survey/main/components/ClosingSurvey.tsx
@@ -21,7 +21,7 @@ import Swal from 'sweetalert2';
 import Avatar from '@mui/material/Avatar';
 import ClearTwoToneIcon from '@mui/icons-material/ClearTwoTone';
 import EventAvailableIcon from '@mui/icons-material/EventAvailable';
-import axios from '../../../login/components/customApi';
+
 import '../../../../global.css';
 import { CardDataListProps, CardDataProps } from '../types/MainType';
 
