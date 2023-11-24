1ae36f250a37623103fbba097995ed85444c87e1
MyeoungDev <lung4536@gmail.com>
2023-11-24 10:59:58 +0900

    [style]: css style 변경 #133

--- src/modules/survey/creation/components/FloatingActionButtons.tsx
+++ src/modules/survey/creation/components/FloatingActionButtons.tsx
@@ -37,16 +37,30 @@ const styles = {
     height: '44px',
     marginBottom: '8px',
     color: 'white',
+    backgroundColor: '#3e3e3e',
+    '&:hover': {
+      backgroundColor: '#6C6B6B',
+    },
   }),
 
   appBar: css({
     position: 'relative',
     paddingRight: 0,
+    backgroundColor: '#3e3e3e',
   }),
 
   previewTitle: css({
     ml: 2,
     flex: 1,
+    textAlign: 'center',
+  }),
+
+  iconButton: css({
+    backgroundColor: '#3e3e3e',
+  }),
+
+  closeIcon: css({
+    color: '#ffffff',
   }),
 };
 const fontFamily = 'GmarketSansMedium';
@@ -121,52 +135,16 @@ function FloatingActionButtons({
 
   return (
     <Box css={styles.fabBox}>
-      <Fab
-        sx={{
-          bgcolor: '#3e3e3e',
-          '&:hover': {
-            backgroundColor: '#6C6B6B', // Change this to the desired color on hover
-          },
-        }}
-        aria-label="add"
-        css={styles.fabStyles}
-        onClick={scrollToTop}
-      >
+      <Fab aria-label="add" css={styles.fabStyles} onClick={scrollToTop}>
         <ArrowDropUpIcon />
       </Fab>
-      <Fab
-        sx={{
-          bgcolor: '#3e3e3e',
-          '&:hover': {
-            backgroundColor: '#6C6B6B', // Change this to the desired color on hover
-          },
-        }}
-        aria-label="add"
-        css={styles.fabStyles}
-        onClick={scrollToBottom}
-      >
+      <Fab aria-label="add" css={styles.fabStyles} onClick={scrollToBottom}>
         <ArrowDropDownIcon />
       </Fab>
-      <Fab
-        sx={{
-          bgcolor: '#3e3e3e',
-          '&:hover': {
-            backgroundColor: '#6C6B6B', // Change this to the desired color on hover
-          },
-        }}
-        aria-label="add"
-        css={styles.fabStyles}
-        onClick={handleAddQuestion}
-      >
+      <Fab aria-label="add" css={styles.fabStyles} onClick={handleAddQuestion}>
         <AddIcon />
       </Fab>
       <Fab
-        sx={{
-          bgcolor: '#3e3e3e',
-          '&:hover': {
-            backgroundColor: '#6C6B6B', // Change this to the desired color on hover
-          },
-        }}
         aria-label="add"
         css={styles.fabStyles}
         onClick={handleOpenPreviewModal}
@@ -179,10 +157,9 @@ function FloatingActionButtons({
         open={isPreviewModalOpen}
         onClose={handleClosePreviewModal}
         TransitionComponent={Transition}
-        sx={{ bgcolor: '#E4E4E4' }}
       >
         <AppBar css={styles.appBar}>
-          <Toolbar sx={{ bgcolor: '#3e3e3e' }}>
+          <Toolbar>
             <Typography
               css={styles.previewTitle}
               fontStyle={textStyle}
@@ -193,11 +170,11 @@ function FloatingActionButtons({
             </Typography>
             <IconButton
               edge="start"
-              sx={{ bgcolor: '#3e3e3e' }}
+              css={styles.iconButton}
               onClick={handleClosePreviewModal}
               aria-label="close"
             >
-              <CloseIcon css={{ color: '#ffffff' }} />
+              <CloseIcon css={styles.closeIcon} />
             </IconButton>
           </Toolbar>
         </AppBar>
