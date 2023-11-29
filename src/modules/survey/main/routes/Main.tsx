71e49409d5d09c4860d0e8ba3174b2e207086f1a
MyeoungDev <lung4536@gmail.com>
2023-11-29 16:22:38 +0900

    [feat]: 공유하기 기능 완료 #158

--- src/modules/survey/main/routes/Main.tsx
+++ src/modules/survey/main/routes/Main.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import { Backdrop, Box, CircularProgress } from '@mui/material';
+import customAxios from 'axios';
 import Floating from '../components/Floating';
 import ClosingSurvey from '../components/ClosingSurvey';
 import RecentSurvey from '../components/RecentSurvey';
@@ -10,6 +11,7 @@ import WeeklySurvey from '../components/WeeklySurvey';
 import '../../../../global.css';
 import { CardDataProps } from '../types/MainType';
 import axios from '../../../login/components/customApi';
+import MainModal from '../components/MainModal';
 
 const fontFamily = 'GmarketSansMedium';
 const textStyle = {
@@ -44,6 +46,9 @@ function Main() {
   const [weeklySurveyLoaded, setWeeklySurveyLoaded] = useState(false);
   const [recentSurveyLoaded, setRecentSurveyLoaded] = useState(false);
   const [closingSurveyLoaded, setClosingSurveyLoaded] = useState(false);
+
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedCard, setSelectedCard] = useState<CardDataProps | null>(null);
 
   const navigate = useNavigate();
 
@@ -90,11 +95,58 @@ function Main() {
     fetchClosingSurveyData();
   }, []);
 
-  const redirectToSurvey = sessionStorage.getItem('redirectToSurvey');
+  /**
+   * 메인애서 RedirectModal 을 닫는 메서드 입니다.
+   *
+   * @author 강명관
+   */
+  const closeCardModal = () => {
+    setSelectedCard(null);
+    setOpenModal(false);
+  };
 
+  const redirectToSurveyNo = sessionStorage.getItem('redirectToSurveyNo');
+
+  /**
+   * 설문 참여에 바로 들어왔을때 메인해서 해당 번호를통해 설문의 모달을 보여주기 위해 모달의 정보를 가져오는 메서드 입니다.
+   *
+   * @auhor 강명관
+   */
+  const fetchSelectedSurveyData = async () => {
+    const axiosForRedirectModal = customAxios.create({
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    });
+
+    try {
+      const response = await axiosForRedirectModal.get(
+        `${process.env.REACT_APP_BASE_URL}/api/surveys/details/${redirectToSurveyNo}`
+      );
+
+      if (response.status !== 200) {
+        return;
+      }
+
+      sessionStorage.clear();
+      setSelectedCard(response.data.content);
+      setOpenModal(true);
+    } catch (error) {
+      console.debug('RedirectModal Error {}', error);
+    }
+  };
+
+  /**
+   * 설문 참여에서 메인으로 리다이렉트 되었을 경우 해당 설문의 모달을 뛰어주기 위한 useEffect 입니다.
+   *
+   * @author 강명관
+   */
   useEffect(() => {
-    console.log(redirectToSurvey);
-  }, [redirectToSurvey]);
+    if (redirectToSurveyNo) {
+      fetchSelectedSurveyData();
+    }
+  }, [redirectToSurveyNo]);
 
   if (!weeklySurveyLoaded && !recentSurveyLoaded && !closingSurveyLoaded) {
     return (
@@ -128,6 +180,14 @@ function Main() {
       <ClosingSurvey cardList={closingSurveyData} />
       <div style={arrowStyle} />
       <Floating />
+
+      {selectedCard && (
+        <MainModal
+          closeCardModal={closeCardModal}
+          openModal={openModal}
+          selectedCard={selectedCard}
+        />
+      )}
     </Container>
   );
 }
