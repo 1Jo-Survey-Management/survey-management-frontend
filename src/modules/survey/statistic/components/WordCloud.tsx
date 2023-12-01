d2187a2cae7cc83eebc20dc6c2f395659bd2f312
PUO <86181292+kl204@users.noreply.github.com>
2023-12-01 18:25:56 +0900

    [fix] 워드클라우드 단어 간격 수정 #202

--- src/modules/survey/statistic/components/WordCloud.tsx
+++ src/modules/survey/statistic/components/WordCloud.tsx
@@ -14,7 +14,7 @@ function updateWordCloudData(
   const existingWord = wordCloudData.find((item) => item.text === newText);
 
   if (existingWord) {
-    existingWord.size += incrementValue + 10;
+    existingWord.size += incrementValue;
   } else {
     wordCloudData.push({ text: newText, size: incrementValue });
   }
@@ -24,14 +24,10 @@ function updateWordCloudData(
 
 const generateRandomPastelColor = (): string => {
   const pastelColors = [
-    '#FFD1DC', // 연한 분홍
-    '#FFA07A', // 연한 주황
-    '#FFB6C1', // 연한 분홍
-    '#FFDEAD', // 연한 베이지
-    '#87CEEB', // 연한 하늘색
-    '#98FB98', // 연한 녹색
-    '#DDA0DD', // 연한 보라
-    '#FFD700', // 연한 금색
+    '#FF6E8B', // 진한 분홍
+    '#FF8C69', // 진한 주황
+    '#9400D3', // 진한 보라
+    '#9ACD32', // 진한 녹색
   ];
 
   return pastelColors[Math.floor(Math.random() * pastelColors.length)];
@@ -46,8 +42,9 @@ function WordCloud({ wordCloud }: WordCloudProps): JSX.Element | null {
 
   const [width] = useState<number>(window.innerWidth < 600 ? 300 : 800);
   const [height] = useState<number>(window.innerWidth < 600 ? 200 : 600);
+  const placedWords = useRef<{ x: number; y: number }[]>([]);
 
-  const incrementValue = 10;
+  const incrementValue = 1;
 
   const updateWordCloud = () => {
     const updatedWordCloudData = wordCloud.reduce(
@@ -55,12 +52,25 @@ function WordCloud({ wordCloud }: WordCloudProps): JSX.Element | null {
       [...wordCloudData]
     );
 
-    setWordCloudData(updatedWordCloudData);
+    updatedWordCloudData.sort((a, b) => b.size - a.size);
+
+    const trimmedWordCloudData = updatedWordCloudData.slice(0, 10);
+
+    setWordCloudData(trimmedWordCloudData);
   };
 
   function calculateSizeBasedOnLength(text: string) {
     return Math.max(10, 30 - text.length * 2);
   }
+  const padding = 150; // 적절한 패딩 값 설정
+  const checkCollision = (x: number, y: number): boolean =>
+    placedWords.current.some(
+      (word) =>
+        x > word.x - padding &&
+        x < word.x + padding &&
+        y > word.y - padding &&
+        y < word.y + padding
+    );
 
   useEffect(() => {
     updateWordCloud();
@@ -84,8 +94,9 @@ function WordCloud({ wordCloud }: WordCloudProps): JSX.Element | null {
               size: d.size,
             }))
           )
-          .padding(5)
-          .rotate(() => Math.random() * 90 - 45);
+          // .padding(20)
+          .spiral('rectangular')
+          .rotate(() => Math.random() * 90);
 
         const wordData = layout.words();
 
@@ -94,19 +105,26 @@ function WordCloud({ wordCloud }: WordCloudProps): JSX.Element | null {
           .data(wordData)
           .enter()
           .append('text')
-          .attr(
-            'transform',
-            () =>
-              `translate(${
+          .attr('transform', () => {
+            // 새로운 단어의 위치를 계산하고 충돌 확인
+            let x;
+            let y;
+            do {
+              x =
                 window.innerWidth <= 600
-                  ? Math.random() * 110 + 105 // 600px 이하일 때
-                  : Math.random() * 200 + 210 // 600px 초과일 때
-              },${
+                  ? Math.random() * 110 + 105
+                  : Math.random() * 600 + 100;
+              y =
                 window.innerWidth <= 600
                   ? Math.random() * 60 + 61
-                  : Math.random() * 200 + 210
-              }) rotate(${Math.random() * 90 - 45})`
-          )
+                  : Math.random() * 400 + 100;
+            } while (checkCollision(x, y)); // 충돌이 발생하면 다시 계산
+
+            // 위치를 저장
+            placedWords.current.push({ x, y });
+
+            return `translate(${x},${y}) `;
+          })
           .style('font-size', `${window.innerWidth <= 600 ? 20 : 70}`)
           .style(
             'font-size',
